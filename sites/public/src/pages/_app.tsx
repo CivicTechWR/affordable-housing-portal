a7281f29f5c9f0facaa091a9a9b2eb29c7c7c29f
Adin Schmidt <9936213+adinschmidt@users.noreply.github.com>
2026-03-05 00:44:00 -0500

    feat: require login to view listings on public site
    Wrap public site in RequireLogin component so unauthenticated users are
    redirected to /sign-in. Auth-related pages are exempted via skipForRoutes.

--- sites/public/src/pages/_app.tsx
+++ sites/public/src/pages/_app.tsx
@@ -16,6 +16,7 @@ import {
   ConfigProvider,
   AuthProvider,
   MessageProvider,
+  RequireLogin,
 } from "@bloom-housing/shared-helpers"
 import { pageChangeHandler, gaLoadScript, gaCaptureScript, uaScript } from "../lib/customScripts"
 import { AppSubmissionContext } from "../lib/applications/AppSubmissionContext"
@@ -29,6 +30,17 @@ import LinkComponent from "../components/core/LinkComponent"
 import "../../styles/overrides.scss"
 
 const rtlLocales = process.env.rtlLanguages.split(",")
+
+const skipLoginRoutes = [
+  "/sign-in",
+  "/create-account",
+  "/forgot-password",
+  "/reset-password",
+  "/users/confirm",
+  "/verify",
+  "/disclaimer",
+  "/privacy",
+]
 
 function PublicApp({ Component, router, pageProps }: AppProps) {
   const { locale } = router
@@ -93,12 +105,18 @@ function PublicApp({ Component, router, pageProps }: AppProps) {
   const pageContent = (
     <ConfigProvider apiUrl={process.env.backendApiBase}>
       <AuthProvider>
-        <MessageProvider>
-          <LoggedInUserIdleTimeout onTimeout={() => conductor.reset()} />
-          <div className={jurisdictionClassname}>
-            <Component {...pageProps} />
-          </div>
-        </MessageProvider>
+        <RequireLogin
+          signInPath="/sign-in"
+          signInMessage={"Login is required to view this page."}
+          skipForRoutes={skipLoginRoutes}
+        >
+          <MessageProvider>
+            <LoggedInUserIdleTimeout onTimeout={() => conductor.reset()} />
+            <div className={jurisdictionClassname}>
+              <Component {...pageProps} />
+            </div>
+          </MessageProvider>
+        </RequireLogin>
       </AuthProvider>
     </ConfigProvider>
   )
