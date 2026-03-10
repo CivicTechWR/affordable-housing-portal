57d54cdb2e8c864e745e9ec793e50dc099838fbc
Jessica Liu <jessica.liu1610@gmail.com>
2026-03-10 15:55:59 -0400

    fix phantom migration date issues

--- api/prisma/migrations/20260305004336_custom_listing_features/migration.sql
+++ api/prisma/migrations/20260305004336_custom_listing_features/migration.sql
@@ -1,16 +1,6 @@
 -- CreateEnum
 CREATE TYPE "custom_listing_scope" AS ENUM ('PROPERTY', 'UNIT');
 
--- AlterTable
-ALTER TABLE "listing_snapshot" ALTER COLUMN "afs_last_run_at" SET DEFAULT '1970-01-01 00:00:00-07'::timestamp with time zone,
-ALTER COLUMN "last_application_update_at" SET DEFAULT '1970-01-01 00:00:00-07'::timestamp with time zone,
-ALTER COLUMN "requested_changes_date" SET DEFAULT '1970-01-01 00:00:00-07'::timestamp with time zone;
-
--- AlterTable
-ALTER TABLE "listings" ALTER COLUMN "afs_last_run_at" SET DEFAULT '1970-01-01 00:00:00-07'::timestamp with time zone,
-ALTER COLUMN "last_application_update_at" SET DEFAULT '1970-01-01 00:00:00-07'::timestamp with time zone,
-ALTER COLUMN "requested_changes_date" SET DEFAULT '1970-01-01 00:00:00-07'::timestamp with time zone;
-
 -- CreateTable
 CREATE TABLE "custom_listing_fields" (
     "id" UUID NOT NULL DEFAULT uuid_generate_v4(),
