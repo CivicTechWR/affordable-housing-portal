91ca57c813dbb3fe5fbf16dbe1ca8a694b09bcac
Adin Schmidt <9936213+adinschmidt@users.noreply.github.com>
2026-03-14 19:24:16 -0400

    fix: update seed listing data to use renamed Canadian address exports

--- api/prisma/seed-helpers/listing-data/sunshine-flats.ts
+++ api/prisma/seed-helpers/listing-data/sunshine-flats.ts
@@ -5,7 +5,7 @@ import {
   ApplicationMethodsTypeEnum,
 } from '@prisma/client';
 import dayjs from 'dayjs';
-import { glacierAddress } from '../address-factory';
+import { jaspAddress } from '../address-factory';
 
 export const sunshineFlats: Prisma.ListingsCreateInput = {
   additionalApplicationSubmissionNotes: null,
@@ -76,7 +76,7 @@ export const sunshineFlats: Prisma.ListingsCreateInput = {
   contentUpdatedAt: new Date(),
   publishedAt: new Date(),
   listingsBuildingAddress: {
-    create: glacierAddress,
+    create: jaspAddress,
   },
   listingsApplicationPickUpAddress: undefined,
   listingsLeasingAgentAddress: undefined,
