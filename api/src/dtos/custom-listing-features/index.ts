81b3b1ac3b033c5726ae06583a03fba3298d9b00
Jessica Liu <jessica.liu1610@gmail.com>
2026-03-18 16:41:36 -0400

    remove old import from custom index, create seed

--- api/src/dtos/custom-listing-features/index.ts
+++ api/src/dtos/custom-listing-features/index.ts
@@ -1,4 +1,3 @@
 export { CustomListingFeature } from './custom-listing-feature.dto';
 export { CustomListingFeatureCreate } from './custom-listing-feature-create.dto';
 export { CustomListingFeatureUpdate } from './custom-listing-feature-update.dto';
-export { CustomFeatureAssociate } from './custom-feature-associate.dto';
