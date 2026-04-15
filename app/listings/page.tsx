7de9a559d446be5fe0ca8e838be8a4775eeee67e
Jessica Liu <jessica.liu1610@gmail.com>
2026-04-14 21:17:29 -0400

    remove searchHeader which is now ListingFilterSearch bar, add carousel and some styling changes

--- app/listings/page.tsx
+++ app/listings/page.tsx
@@ -1,12 +1,14 @@
-// app/listings/page.tsx
-// types/listings.ts
-import { NuqsAdapter } from 'nuqs/adapters/next/app'
-import ListingsDashboard from './listings';
+import { Suspense } from "react";
+import { NuqsAdapter } from "nuqs/adapters/next/app";
+import ListingsDashboard from "./listings";
+import ListingsSkeleton from "@/components/listings-skeleton/ListingsSkeleton";
 
 export default async function ListingsPage() {
-    return (
-        <NuqsAdapter>
-            <ListingsDashboard />
-        </NuqsAdapter>
-    );
+  return (
+    <NuqsAdapter>
+      <Suspense fallback={<ListingsSkeleton />}>
+        <ListingsDashboard />
+      </Suspense>
+    </NuqsAdapter>
+  );
 }
