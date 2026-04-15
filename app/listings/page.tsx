c93491119e4fdaa257298bac900b26bcf8b932ae
Jessica Liu <jessica.liu1610@gmail.com>
2026-04-15 15:58:47 -0400

    fix import

--- app/listings/page.tsx
+++ app/listings/page.tsx
@@ -1,12 +1,11 @@
 import { Suspense } from "react";
 import { NuqsAdapter } from "nuqs/adapters/next/app";
 import ListingsDashboard from "./listings";
-import ListingsSkeleton from "@/components/listings-skeleton/ListingsSkeleton";
 
 export default async function ListingsPage() {
   return (
     <NuqsAdapter>
-      <Suspense fallback={<ListingsSkeleton />}>
+      <Suspense>
         <ListingsDashboard />
       </Suspense>
     </NuqsAdapter>
